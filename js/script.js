c888abdb7a81328dc0c143e43b5f91036c9cd73f
andr3w <akeir@pm.me>
2022-09-19 22:04:17 +1000

    before i break it trying to nest this loop

--- js/script.js
+++ js/script.js
@@ -1,14 +1,53 @@
-let currentPlayer = 'player1'
+//==================================================
+//Game Variables
+//==================================================
+
+let currentPlayer = 'X'
 const board = document.querySelector('.board')
+let p1 = []
+let p2 = []
+
+
+const hyperBitcoinization = document.querySelector('.btc-mode')
+//==================================================
+//Game Functions
+//==================================================
+
+// event listener identifies where grid is clicked
 
 board.addEventListener('click', (event) => {
     if (event.target.textContent) {
-        console.log('choose another square');
-    } else if (currentPlayer === 'player1') {
+        alert('choose a different square');
+    } else if (currentPlayer === 'X') {
         event.target.textContent = 'X'
-        currentPlayer = 'player2'
+        currentPlayer = 'O'
+        // console.log(currentPlayer);
+        const num = Number(event.target.dataset.index);
+        // console.log(num);
+        p1.push(num)
+        // console.log(p1);
     } else {
         event.target.textContent = 'O'
-        currentPlayer = 'player1'
+        currentPlayer = 'X'
+        // console.log(currentPlayer);
+        const num = Number(event.target.dataset.index);
+        // console.log(num);
+        p2.push(num)
+        // console.log(p2);
     } 
 })
+
+// WHO WINS ?
+
+for (i = 0; i < p1.length; i++) {
+    console.log(p1);
+
+}
+
+
+// hyperBitcoinization.addEventListener('click', (event) => {
+//     if (event.target) {
+        
+//     }
+
+// })
--- plan.md
+++ plan.md
@@ -10,3 +10,23 @@ Theme will be bitcoin influenced. Enable toggle on/off for bitcoin-mode.
 - RGB: 242, 169, 0
 - #4d4d4e
 
+To do:
+
+- clicking the bitcoin button enables the bitcoin logo instead of O's.
+
+- identify if someone wins.
+- show winner.
+- provide a button to reset the game
+- update the players score
+- build board as an array when each element is selected
+- add style when user hovers over spare. shade background color.
+- 
+
+Winning combinations
+123
+159
+357
+147
+369
+789
+
